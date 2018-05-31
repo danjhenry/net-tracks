1b6d8aee730c34e769e78d8d63a1869e9b896bb6
Daniel Henry <email378637@gmail.com>
2018-05-31 09:37:49 -0700

    Add files via upload

---
+++ net tracks python/net_client.py
@@ -0,0 +1,45 @@
+import requests
+from win32 import win32api
+import time
+
+url = 'http://192.168.8.108/controls'
+
+def keyState(keys):
+    stack = []
+    for key in keys:
+        state = win32api.GetAsyncKeyState(ord(key))
+        if(state < 0 or state == 1):
+            stack.append(key)
+    return stack
+
+def main():
+    state = False
+    payload = {}
+    while True:
+        keysOn = keyState('WASD')
+        if(keysOn):
+            state = True
+            payload['Relay'] = 2
+            payload['steerAngle'] = 68
+            for key in keysOn:
+                if(key == 'W'):
+                    payload['left_bank'] = 1
+                    payload['right_bank'] = 1
+                if(key == 'S'):
+                    payload['left_bank'] = 0
+                    payload['right_bank'] = 0
+                if(key == 'A'):
+                    payload['left_bank'] = 1
+                    payload['right_bank'] = 0
+                if(key == 'D'):
+                    payload['left_bank'] = 0
+                    payload['right_bank'] = 1
+            r = requests.get(url, params=payload)
+            print(r.text, '\n')
+        elif state:
+            state = False
+            payload['left_bank'] = 2
+            payload['right_bank'] = 2
+            r = requests.get(url, params=payload)
+            print(r.text, '\n')
+main()
